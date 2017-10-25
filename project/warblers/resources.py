b5c79ba8b1583156e9ae1cca1ab7c1ea7d955047
Kevin Chang <kevincchang13@gmail.com>
2017-10-24 17:10:21 -0700

    fix bug

--- project/warblers/resources.py
+++ project/warblers/resources.py
@@ -8,25 +8,22 @@ warblers_api = Api(warbler_blueprint)
 
 # @warblers_api.resource('/')
 class WarblersAPI(Resource):
-    def get(self): #get all wablererss
-        pass
-
-class WarblerAPI(Resource):
     def post(self, user_id): #create new wablereressss
         content = request.get_json()
         warble = Warbler(content['message'], user_id)
         db.session.add(warble)
         db.session.commit()
         return
+    def get(self): #get all wablererss
+        pass
 
+class WarblerAPI(Resource):
     def get(self, warbler_id, user_id):
         warble = Warbler.query.get_or_404(warbler_id)
         return make_response(jsonify(warble))
         
-
     def delete(self, warbler_id, user_id):
         pass
 
-warblers_api.add_resource(WarblersAPI, '')
-warblers_api.add_resource(WarblerAPI, '/<string:user_id>')
+warblers_api.add_resource(WarblersAPI, '/<string:user_id>')
 warblers_api.add_resource(WarblerAPI, '/<string:user_id>/<string:warbler_id>')
