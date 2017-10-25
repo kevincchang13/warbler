3d132993a920708a820b4e7e000bb45a06c76bec
Kevin Chang <kevincchang13@gmail.com>
2017-10-25 11:44:50 -0700

    added auth api

--- project/users/models.py
+++ project/users/models.py
@@ -8,23 +8,14 @@ class User(db.Model):
     username = db.Column(db.Text, unique=True)
     name = db.Column(db.Text)
     password = db.Column(db.Text)
-    messages = db.relationship(
-        'User',
-        lazy='dynamic',
-        backref=db.backref('user')
-    )
+    # messages = db.relationship(
+    #     'User',
+    #     lazy='dynamic',
+    #     backref=db.backref('user')
+    # )
 
     def __init__(self, email, username, name, password):
         self.email = email
         self.username = username
         self.name = name
         self.password = bcrypt.generate_password_hash(password).decode('UTF-8')
-
-    @classmethod
-    def authenticate(cls, username, password):
-        user = cls.query.filter_by(username=username).first()
-        if user:
-            authenticated_user=bcrypt.check_password_hash(user.password, password)
-            if authenticated_user:
-                return user
-        return False
